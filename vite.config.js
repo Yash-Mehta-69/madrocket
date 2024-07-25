55a05525ba94573d52d60cfb41f6f14b253dc178
Yash-Mehta-69 <yashm8866@gmail.com>
2024-07-25 19:45:19 +0530

    chnage

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/madrocket/', // The base path should match your repository name
+  base: '/madrocket/',
   plugins: [react()]
 });
